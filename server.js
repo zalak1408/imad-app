7b779ebb483b429a49f5373d2fbfd1373d13a310
zalak1408 <36542675+zalak1408@users.noreply.github.com>
2018-02-22 20:49:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var counter=0;
-app.ge('/counter',function(req,res){
+app.get('/counter',function(req,res){
     counter=counter+1;
      res.send(counter.toString());
 }) ;
