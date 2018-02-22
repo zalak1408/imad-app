4cae442454e82023925c0f63a23053a0a7ffe7b9
zalak1408 <36542675+zalak1408@users.noreply.github.com>
2018-02-22 19:58:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
 var img=document.getElementById('madi');
-madi.onclick=function(){
+img.onclick=function(){
     img.style.marginleft='100px';
 };
